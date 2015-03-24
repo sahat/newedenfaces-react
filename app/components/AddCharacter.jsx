a0322d6e823bc5c24c3d57eb6bf97f07e648cf1c
Sahat Yalkabov <sakhat@gmail.com>
2015-03-24 10:40:47 -0700

    added  message and class on success, clear it on input change

--- app/components/AddCharacter.jsx
+++ app/components/AddCharacter.jsx
@@ -1,6 +1,43 @@
 var React = require('react');
 
 var AddCharacter = React.createClass({
+
+  getInitialState: function() {
+    return {
+      helpBlock: ''
+    }
+  },
+
+  handleNameChange: function(event) {
+    if (event.target.parentNode.classList.contains('has-success')) {
+      event.target.parentNode.classList.remove('has-success');
+      this.setState({ helpBlock: '' });
+    }
+    this.setState({ name: event.target.value })
+  },
+
+  handleGenderChange: function(event) {
+    this.setState({ gender: event.target.value })
+  },
+
+  handleSubmit: function(event) {
+    event.preventDefault();
+
+    var name = this.state.name.trim();
+    var gender = this.state.gender;
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/characters',
+      data: { name: name, gender: gender }
+    }).done(function() {
+      this.refs.name.getDOMNode().value = '';
+      this.refs.name.getDOMNode().focus();
+      this.refs.name.getDOMNode().parentNode.classList.add('has-success');
+      this.setState({ helpBlock: name + ' has been added successfully!' });
+    }.bind(this));
+  },
+
   render: function() {
     return (
       <div className="row flipInX animated">
@@ -8,23 +45,23 @@ var AddCharacter = React.createClass({
           <div className='panel panel-default'>
             <div className='panel-heading font-bold'>Add Character</div>
             <div className='panel-body'>
-              <form role='form'>
+              <form onSubmit={this.handleSubmit}>
                 <div className='form-group'>
-                  <label>Character Name</label>
-                  <input type='text' className='form-control' autoFocus />
-                  <span className="help-block"></span>
+                  <label className='control-label'>Character Name</label>
+                  <input type='text' className='form-control' ref='name' onChange={this.handleNameChange} autoFocus/>
+                  <span className="help-block">{this.state.helpBlock}</span>
                 </div>
                 <div className='form-group'>
                   <div className='radio radio-inline'>
-                    <input type='radio' name='gender' id='female' value='female' />
+                    <input type='radio' name='gender' id='female' value='female' onChange={this.handleGenderChange}/>
                     <label htmlFor='female'>Female</label>
                   </div>
                   <div className='radio radio-inline'>
-                    <input type='radio' name='gender' id='male' value='male' />
+                    <input type='radio' name='gender' id='male' value='male' onChange={this.handleGenderChange}/>
                     <label htmlFor='male'>Male</label>
                   </div>
                 </div>
-                <button type='submit' className='btn btn-primary'>Submit</button>
+                <button type='submit' className='btn btn-primary'>Success</button>
               </form>
             </div>
           </div>
