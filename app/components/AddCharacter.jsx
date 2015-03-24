d66abd58072ace04e5466c98b85dad2744f0732d
Sahat Yalkabov <sakhat@gmail.com>
2015-03-23 22:42:52 -0700

    added UI for AaddCharacter component

--- app/components/AddCharacter.jsx
+++ app/components/AddCharacter.jsx
@@ -3,8 +3,31 @@ var React = require('react');
 var AddCharacter = React.createClass({
   render: function() {
     return (
-      <div className="container">
-        AddCharacter Page
+      <div className="row">
+        <div className="col-sm-8">
+          <div className='panel panel-default'>
+            <div className='panel-heading font-bold'>Add Character</div>
+            <div className='panel-body'>
+              <form role='form'>
+                <div className='form-group'>
+                  <label>Character Name</label>
+                  <input type='text' className='form-control' autoFocus />
+                </div>
+                <div className='form-group'>
+                  <div className='radio radio-inline'>
+                    <input type='radio' name='gender' id='female' value='female' />
+                    <label htmlFor='female'>Female</label>
+                  </div>
+                  <div className='radio radio-inline'>
+                    <input type='radio' name='gender' id='male' value='male' />
+                    <label htmlFor='male'>Male</label>
+                  </div>
+                </div>
+                <button type='submit' className='btn btn-primary'>Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
