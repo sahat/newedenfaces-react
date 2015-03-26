fc3a5fa5e1c287303507be942d7dfacb0e8458b8
Sahat Yalkabov <sakhat@gmail.com>
2015-03-25 23:07:57 -0700

    added click handler for character voting

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -14,12 +14,29 @@ var Home = React.createClass({
     }.bind(this));
   },
 
+  handleClick: function(character) {
+    var winner = character;
+    var loser = this.state.characters[1 - this.state.characters.indexOf(winner)];
+
+    $.ajax({
+      type: 'PUT',
+      url: '/api/characters',
+      data: { winner: winner.characterId, loser: loser.characterId },
+      success: function() {
+        $.get('/api/characters', function(data) {
+          this.setState({ characters: data });
+        }.bind(this));
+      }.bind(this)
+    });
+  },
+
   render: function() {
     var characterNodes = this.state.characters.map(function(character, index) {
       return (
         <div key={character.characterId} className={index === 0 ? 'col-xs-6 col-sm-6 col-md-5 col-md-offset-1' : 'col-xs-6 col-sm-6 col-md-5'}>
           <div className='thumbnail fadeInUp animated'>
-            <img src={'http://image.eveonline.com/Character/' + character.characterId + '_512.jpg'} />
+            <img onClick={this.handleClick.bind(this, character)} src={'http://image.eveonline.com/Character/' + character.characterId + '_512.jpg'}/>
+
             <div className='caption text-center'>
               <ul className='list-inline'>
                 <li><strong>Race:</strong> {character.race}</li>
@@ -32,11 +49,12 @@ var Home = React.createClass({
           </div>
         </div>
       );
-    });
+    }.bind(this));
 
     return (
       <div className='container'>
         <h3 className='text-center'>Click on the portrait. Select your favorite.</h3>
+
         <div className='row'>
           {characterNodes}
         </div>
