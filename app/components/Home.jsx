aa8fe321dd775a9cae6af881ce50a62333dd6497
Sahat Yalkabov <sakhat@gmail.com>
2015-03-25 21:15:38 -0700

    fetch characters on home page from server

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,35 +1,44 @@
 var React = require('react');
 
 var Home = React.createClass({
+
+  getInitialState: function() {
+    return {
+      characters: []
+    }
+  },
+
+  componentDidMount: function() {
+    $.get('/api/characters', function(data) {
+      this.setState({ characters: data });
+    }.bind(this));
+  },
+
   render: function() {
+    var characterNodes = this.state.characters.map(function(character, index) {
+      return (
+        <div className={index === 0 ? 'col-xs-6 col-sm-6 col-md-5 col-md-offset-1' : 'col-xs-6 col-sm-6 col-md-5'}>
+          <div className='thumbnail fadeInUp animated'>
+            <img src={'http://image.eveonline.com/Character/' + character.characterId + '_512.jpg'} />
+            <div className='caption text-center'>
+              <ul className='list-inline'>
+                <li><strong>Race:</strong> {character.race}</li>
+                <li><strong>Bloodline:</strong> {character.bloodline}</li>
+              </ul>
+              <h4>
+                <a href={'/characters/' + character.characterId}><strong>{character.name}</strong></a>
+              </h4>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
     return (
-      <div className="container">
-        <h3 className="text-center">Click on the portrait. Select your favorite.</h3>
-        <div className="row">
-          <div className="col-xs-6 col-sm-6 col-md-5 col-md-offset-1">
-            <div className="thumbnail fadeInUp animated">
-              <img src="http://image.eveonline.com/Character/230694467_512.jpg"/>
-              <div className="caption text-center">
-                <ul className="list-inline">
-                  <li><strong>Race:</strong> Caldari</li>
-                  <li><strong>Bloodline:</strong> Civire</li>
-                </ul>
-                <h4><a href="#characters/94194586"><strong>Don Cristo Guerrero</strong></a></h4>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6 col-sm-6 col-md-5">
-            <div className="thumbnail fadeInUp animated">
-              <img src="http://image.eveonline.com/Character/723304069_512.jpg"/>
-              <div className="caption text-center">
-                <ul className="list-inline">
-                  <li><strong>Race:</strong> Caldari</li>
-                  <li><strong>Bloodline:</strong> Civire</li>
-                </ul>
-                <h4><a href="#characters/94194586"><strong>Don Cristo Guerrero</strong></a></h4>
-              </div>
-            </div>
-          </div>
+      <div className='container'>
+        <h3 className='text-center'>Click on the portrait. Select your favorite.</h3>
+        <div className='row'>
+          {characterNodes}
         </div>
       </div>
     );
