00509e0fbc33cc042d3c2581b7000eeaa6f85c7d
Sahat Yalkabov <sakhat@gmail.com>
2015-03-29 15:29:28 -0700

    cleaned up character component, added fade-in animation

--- app/components/Character.jsx
+++ app/components/Character.jsx
@@ -2,6 +2,9 @@ var React = require('react');
 
 
 var Character = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.func.isRequired
+  },
 
   getInitialState: function() {
     return {
@@ -15,33 +18,24 @@ var Character = React.createClass({
     }
   },
 
-  contextTypes: {
-    router: React.PropTypes.func.isRequired
-  },
-
   componentDidMount: function() {
     var routeParams = this.context.router.getCurrentParams();
 
-    $.get('/api/characters/' + routeParams.id, function(data) {
-      document.body.classList.add('profile');
-      document.body.classList.add(data.race.toLowerCase());
+    $.ajax({ url: '/api/characters/' + routeParams.id })
+      .done(function(data) {
+        document.body.classList.add('profile');
+        document.body.classList.add(data.race.toLowerCase());
 
-      var winLossRatio = (data.wins / (data.wins + data.losses) * 100).toFixed(1);
-
-      if (isNaN(winLossRatio)) {
-        winLossRatio = 0;
-      }
-
-      this.setState({
-        characterId: data.characterId,
-        race: data.race,
-        bloodline: data.bloodline,
-        gender: data.gender.charAt(0).toUpperCase() + data.gender.substring(1),
-        wins: data.wins,
-        losses: data.losses,
-        winLossRatio: winLossRatio
-      });
-    }.bind(this));
+        this.setState({
+          characterId: data.characterId,
+          race: data.race,
+          bloodline: data.bloodline,
+          gender: data.gender.charAt(0).toUpperCase() + data.gender.substring(1),
+          wins: data.wins,
+          losses: data.losses,
+          winLossRatio: data.winLossRatio
+        });
+      }.bind(this));
   },
 
   componentWillUnmount: function() {
@@ -53,7 +47,7 @@ var Character = React.createClass({
 
   render: function() {
     return (
-      <div className='container'>
+      <div className='container animated fadeIn'>
         <div className='profile-img'>
           <a className='magnific-popup fade-zoom' href={'https://image.eveonline.com/Character/' + this.state.characterId + '_1024.jpg'}>
             <img src={'https://image.eveonline.com/Character/' + this.state.characterId + '_256.jpg'}/>
@@ -64,7 +58,8 @@ var Character = React.createClass({
           <h4 className='lead'>Race: <strong>{this.state.race}</strong></h4>
           <h4 className='lead'>Bloodline: <strong>{this.state.bloodline}</strong></h4>
           <h4 className='lead'>Gender: <strong>{this.state.gender}</strong></h4>
-          <div className="btn-group">
+
+          <div className='btn-group'>
             <button className='btn btn-transparent'>Request to Delete</button>
             <button className='btn btn-transparent'>Wrong Gender</button>
           </div>
