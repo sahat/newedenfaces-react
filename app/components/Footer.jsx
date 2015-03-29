2e27e70205ce21ff115b594bd76ad222c548e7fb
Sahat Yalkabov <sakhat@gmail.com>
2015-03-28 19:21:36 -0700

    footer now includes top5 leaderboard

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,13 +1,50 @@
 var React = require('react');
+var Router = require('react-router');
+
+var Link = Router.Link;
 
 var Footer = React.createClass({
+
+  getInitialState: function() {
+    return {
+      characters: []
+    }
+  },
+
+  componentDidMount: function() {
+    $.get('/api/leaderboard', function(data) {
+      this.setState({ characters: data });
+    }.bind(this));
+  },
+
   render: function() {
+    var leaderboardCharacters = this.state.characters.map(function(character) {
+      return (
+        <li key={character.characterId}>
+          <Link to={'/characters/' + character.characterId}>
+            <img className='thumb-md' src={'http://image.eveonline.com/Character/' + character.characterId + '_128.jpg'} />
+          </Link>
+        </li>
+      )
+    });
+
     return (
       <footer>
-        <div className="container">
-          <h3><strong>Information</strong> and <strong>Copyright</strong></h3>
-          <p>© 2015 Sahat Yalkabov.</p>
-          <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong>.</p>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-5'>
+              <h3 className='lead'><strong>Information</strong> and <strong>Copyright</strong></h3>
+              <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong>.</p>
+              <p>You may view the <a href='https://github.com/sahat/newedenfaces-react'>Source Code</a> behind this project on GitHub</p>
+              <p>© 2015 Sahat Yalkabov.</p>
+            </div>
+            <div className='col-sm-7 hidden-xs'>
+              <h3 className='lead'><strong>Leaderboard</strong> Top 5 Characters</h3>
+              <ul className='list-inline'>
+                {leaderboardCharacters}
+              </ul>
+            </div>
+          </div>
         </div>
       </footer>
     );
