dbf78eb029160759568bf8a6cfa564ec0053dec3
Sahat Yalkabov <sakhat@gmail.com>
2015-03-28 17:28:27 -0700

    refetch characters on characterlist component update

--- app/components/CharacterList.jsx
+++ app/components/CharacterList.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var Router = require('react-router');
 
 var CharacterList = React.createClass({
 
@@ -12,18 +13,57 @@ var CharacterList = React.createClass({
     router: React.PropTypes.func.isRequired
   },
 
-  componentDidMount: function() {
-    $.get('/api/characters/top', function(data) {
+  fetchCharacters: function() {
+    var routeParams = this.context.router.getCurrentParams();
+    var currentPath = this.context.router.getCurrentPath();
+    var baseUrl = '/api/characters/top';
+
+    var options = {
+      race: routeParams.race,
+      bloodline: routeParams.bloodline
+    };
+
+    if (currentPath.indexOf('female') > -1) {
+      options.gender = 'female';
+    } else if (currentPath.indexOf('male') > -1) {
+      options.gender = 'male';
+    }
+
+    $.get(baseUrl, options, function(data) {
       this.setState({ characters: data });
     }.bind(this));
   },
 
+  componentDidMount: function() {
+    this.fetchCharacters();
+    this.setState({
+      path: this.context.router.getCurrentPath()
+    });
+  },
+
+
+  componentDidUpdate: function() {
+    var currentPath = this.context.router.getCurrentPath();
+
+    if (this.state.path === currentPath) {
+      return;
+    }
+    this.fetchCharacters();
+    this.setState({
+      path: currentPath
+    });
+  },
+
   render: function() {
+    console.log();
+    console.log(this.context.router.getCurrentParams());
+
     var characterList = this.state.characters.map(function(character, index) {
       return (
         <div key={character.characterId} className='list-group-item clearfix'>
           <div className='media'>
             <span className='position pull-left'>{index + 1}</span>
+
             <div className='pull-left thumb-lg'>
               <a href={'/characters/' + character.characterId}>
                 <img className='media-object' src={'http://image.eveonline.com/Character/' + character.characterId + '_128.jpg'}/>
