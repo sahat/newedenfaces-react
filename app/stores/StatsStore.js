ef59daf3bfba35b97d729d11b6be9e2bcf9a09c4
Sahat Yalkabov <sakhat@gmail.com>
2015-06-06 05:55:38 -0700

    stats store with success/error handling

---
+++ app/stores/StatsStore.js
@@ -0,0 +1,29 @@
+import {assign} from 'underscore';
+import alt from '../alt';
+import StatsActions from '../actions/StatsActions';
+
+class StatsStore {
+  constructor() {
+    this.bindActions(StatsActions);
+    this.leadingRace = { race: 'Unknown', count: 0 };
+    this.leadingBloodline = { bloodline: 'Unknown', count: 0 };
+    this.amarrCount = 0;
+    this.caldariCount = 0;
+    this.gallenteCount = 0;
+    this.minmatarCount = 0;
+    this.totalVotes = 0;
+    this.femaleCount = 0;
+    this.maleCount = 0;
+    this.totalCount = 0;
+  }
+
+  onGetStatsSuccess(data) {
+    assign(this, data);
+  }
+
+  onGetStatsFail(jqXhr) {
+    toastr.error(jqXhr.responseText || jqXhr.statusText);
+  }
+}
+
+export default alt.createStore(StatsStore);
