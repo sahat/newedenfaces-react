9f87b79031d3e509806578cff53d988e0687da8c
Sahat Yalkabov <sakhat@gmail.com>
2015-06-06 05:56:09 -0700

    converted stats component to flux

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -1,30 +1,25 @@
 import React from 'react';
+import StatsStore from '../stores/StatsStore'
+import StatsActions from '../actions/StatsActions';
 
 class Stats extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-      stats: {
-        leadingRace: { race: 'TBD', count: 0 },
-        leadingBloodline: { bloodline: 'TBD', count: 0 },
-        amarrCount: 0,
-        caldariCount: 0,
-        gallenteCount: 0,
-        minmatarCount: 0,
-        totalVotes: 0,
-        femaleCount: 0,
-        maleCount: 0,
-        totalCount: 0
-      }
-    }
+    this.state = StatsStore.getState();
+    this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
-    $.ajax({ url: '/api/stats' })
-      .done(function(data) {
-        this.setState({ stats: data });
-      }.bind(this));
+    StatsStore.listen(this.onChange);
+    StatsActions.getStats();
+  }
+
+  componentWillUnmount() {
+    StatsStore.unlisten(this.onChange);
+  }
+
+  onChange() {
+    this.setState(StatsStore.getState());
   }
 
   render() {
@@ -40,43 +35,44 @@ class Stats extends React.Component {
             <tbody>
             <tr>
               <td>Leading race in Top 100</td>
-              <td>{this.state.stats.leadingRace.race} with {this.state.stats.leadingRace.count} characters</td>
+              <td>{this.state.leadingRace.race} with {this.state.leadingRace.count} characters</td>
             </tr>
             <tr>
               <td>Leading bloodline in Top 100</td>
-              <td>{this.state.stats.leadingBloodline.bloodline} with {this.state.stats.leadingBloodline.count} characters</td>
+              <td>{this.state.leadingBloodline.bloodline} with {this.state.leadingBloodline.count} characters
+              </td>
             </tr>
             <tr>
               <td>Amarr Characters</td>
-              <td>{this.state.stats.amarrCount}</td>
+              <td>{this.state.amarrCount}</td>
             </tr>
             <tr>
               <td>Caldari Characters</td>
-              <td>{this.state.stats.caldariCount}</td>
+              <td>{this.state.caldariCount}</td>
             </tr>
             <tr>
               <td>Gallente Characters</td>
-              <td>{this.state.stats.gallenteCount}</td>
+              <td>{this.state.gallenteCount}</td>
             </tr>
             <tr>
               <td>Minmatar Characters</td>
-              <td>{this.state.stats.minmatarCount}</td>
+              <td>{this.state.minmatarCount}</td>
             </tr>
             <tr>
               <td>Total votes cast</td>
-              <td>{this.state.stats.totalVotes}</td>
+              <td>{this.state.totalVotes}</td>
             </tr>
             <tr>
               <td>Female characters</td>
-              <td>{this.state.stats.femaleCount}</td>
+              <td>{this.state.femaleCount}</td>
             </tr>
             <tr>
               <td>Male characters</td>
-              <td>{this.state.stats.maleCount}</td>
+              <td>{this.state.maleCount}</td>
             </tr>
             <tr>
               <td>Total number of characters</td>
-              <td>{this.state.stats.totalCount}</td>
+              <td>{this.state.totalCount}</td>
             </tr>
             </tbody>
           </table>
