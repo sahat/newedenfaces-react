cd341d9ecfd8a54894dc261074695f2dc7218673
Sahat Yalkabov <sakhat@gmail.com>
2015-06-06 05:55:06 -0700

    added actions for stats component

---
+++ app/actions/StatsActions.js
@@ -0,0 +1,22 @@
+import alt from '../alt';
+
+class StatsActions {
+  constructor() {
+    this.generateActions(
+      'getStatsSuccess',
+      'getStatsFail'
+    );
+  }
+
+  getStats() {
+    $.ajax({ url: '/api/stats' })
+      .done(data => {
+        this.actions.getStatsSuccess(data);
+      })
+      .fail(jqXhr => {
+        this.actions.getStatsFail(jqXhr);
+      });
+  }
+}
+
+export default alt.createActions(StatsActions);
