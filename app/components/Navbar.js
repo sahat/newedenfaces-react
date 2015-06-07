a4956be8bc99845c5476cb7f261d719eb312c1c0
Sahat Yalkabov <sakhat@gmail.com>
2015-06-06 22:54:20 -0700

    add bind(this) to navbar submit form handler

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -80,11 +80,11 @@ class Navbar extends React.Component {
           </Link>
         </div>
         <div id='navbar' className='navbar-collapse collapse'>
-          <form ref='searchForm' className='navbar-form navbar-left animated' onSubmit={this.handleSubmit}>
+          <form ref='searchForm' className='navbar-form navbar-left animated' onSubmit={this.handleSubmit.bind(this)}>
             <div className='input-group'>
               <input type='text' className='form-control' placeholder={this.state.totalCharacters + ' characters'} value={this.state.searchQuery} onChange={NavbarActions.updateSearchQuery} />
               <span className='input-group-btn'>
-                <button className='btn btn-default' onClick={this.handleSubmit}><span className='glyphicon glyphicon-search'></span></button>
+                <button className='btn btn-default' onClick={this.handleSubmit.bind(this)}><span className='glyphicon glyphicon-search'></span></button>
               </span>
             </div>
           </form>
