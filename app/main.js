2d6b6c1840a80b8ffbf5c91335da193da5b421d9
Sahat Yalkabov <sakhat@gmail.com>
2015-06-20 22:55:55 -0700

    render app into #app container

--- app/main.js
+++ app/main.js
@@ -3,5 +3,5 @@ import Router from 'react-router';
 import routes from './routes';
 
 Router.run(routes, Router.HistoryLocation, function(Handler) {
-  React.render(<Handler />, document);
+  React.render(<Handler />, document.getElementById('app'));
 });
