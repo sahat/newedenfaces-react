ef57ecf60a9e3e56b901392295cb09a9935d1eb2
Sahat Yalkabov <sakhat@gmail.com>
2015-07-19 02:17:23 -0700

    simplified react routes, removed 404

--- app/routes.js
+++ app/routes.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import {Route, NotFoundRoute} from 'react-router';
+import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Stats from './components/Stats';
 import Character from './components/Character';
 import CharacterList from './components/CharacterList';
 import AddCharacter from './components/AddCharacter';
-import NotFound from './components/404';
 
 export default (
   <Route handler={App}>
-    <NotFoundRoute handler={NotFound}/>
     <Route path='/' handler={Home} />
     <Route path='/stats' handler={Stats} />
     <Route path='/characters/:id' handler={Character} />
-    <Route path='/top' handler={CharacterList}>
-      <Route path=':race' handler={CharacterList}>
-        <Route path=':bloodline' handler={CharacterList} />
-      </Route>
-    </Route>
-    <Route path='/female' handler={CharacterList}>
-      <Route path=':race' handler={CharacterList}>
-        <Route path=':bloodline' handler={CharacterList} />
-      </Route>
-    </Route>
-    <Route path='/male' handler={CharacterList}>
+    <Route path=':category' handler={CharacterList}>
       <Route path=':race' handler={CharacterList}>
         <Route path=':bloodline' handler={CharacterList} />
       </Route>
