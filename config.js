89817cc4ac6b147da3ded688978864684e782eac
Sahat Yalkabov <sakhat@gmail.com>
2015-07-19 22:28:57 -0700

    updated package.json description and cleaned up config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  database: process.env.DATABASE || 'localhost:27017',
-  sendgrid: {
-    username:  process.env.SENDGRID_USERNAME || 'airpair_sahat',
-    password: process.env.SENDGRID_PASSWORD || 'airpair2'
-  }
+  database: process.env.MONGO_URI || 'localhost'
 };
