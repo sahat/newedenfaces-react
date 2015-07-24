a90c3c1d2f3dc45f604278d80332656a742ad7e5
Sahat Yalkabov <sakhat@gmail.com>
2015-07-23 20:02:10 -0700

    added missing semicolon

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-import NavbarStore from '../stores/NavbarStore'
+import NavbarStore from '../stores/NavbarStore';
 import NavbarActions from '../actions/NavbarActions';
 
 class Navbar extends React.Component {
