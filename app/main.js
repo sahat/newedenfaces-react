d581dfee15e5c4e56f2fc7bec365ab1a3f615698
Sahat Yalkabov <sakhat@gmail.com>
2015-10-19 15:36:47 -0700

    updated main.js to react-dom 0.14 and react-router 1.0

--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Router from 'react-router';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
+import Navbar from './components/Navbar';
 
-Router.run(routes, Router.HistoryLocation, function(Handler) {
-  React.render(<Handler />, document.getElementById('app'));
-});
+let history = createBrowserHistory();
+
+ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('app'));
