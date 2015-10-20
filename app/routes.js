8d843e4e6b6d02938c9f4d5e67b868312899a4a4
Sahat Yalkabov <sakhat@gmail.com>
2015-10-20 00:28:40 -0700

    removed redundant Router component wrapper

--- app/routes.js
+++ app/routes.js
@@ -8,17 +8,15 @@ import CharacterList from './components/CharacterList';
 import AddCharacter from './components/AddCharacter';
 
 export default (
-  <Router>
-    <Route component={App}>
-      <Route path='/' component={Home} />
-      <Route path='/stats' component={Stats} />
-      <Route path='/characters/:id' component={Character} />
-      <Route path='/add' component={AddCharacter} />
-      <Route path=':category' component={CharacterList}>
-        <Route path=':race' component={CharacterList}>
-          <Route path=':bloodline' component={CharacterList} />
-        </Route>
+  <Route component={App}>
+    <Route path='/' component={Home} />
+    <Route path='/stats' component={Stats} />
+    <Route path='/characters/:id' component={Character} />
+    <Route path='/add' component={AddCharacter} />
+    <Route path=':category' component={CharacterList}>
+      <Route path=':race' component={CharacterList}>
+        <Route path=':bloodline' component={CharacterList} />
       </Route>
     </Route>
-  </Router>
+  </Route>
 );
