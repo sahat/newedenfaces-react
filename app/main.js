c1797dc72fd04fe29b138de57bf5179b7c5f04ae
danjonescidtrix <dannyjones360@me.com>
2016-12-15 15:31:34 +0000

    Removed Navbar import from main.js

--- app/main.js
+++ app/main.js
@@ -3,7 +3,6 @@ import Router from 'react-router';
 import ReactDOM from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
-import Navbar from './components/Navbar';
 
 let history = createBrowserHistory();
 
